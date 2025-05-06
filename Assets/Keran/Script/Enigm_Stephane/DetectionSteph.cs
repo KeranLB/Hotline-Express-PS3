6d3d255c7b27ee78ca245e5d6010e3010cb6c0c3
Keran LE BERRE <148327037+KeranLB@users.noreply.github.com>
2025-05-06 17:40:18 +0200

    tableau prefab

--- Assets/Keran/Script/Enigm_Stephane/DetectionSteph.cs
+++ Assets/Keran/Script/Enigm_Stephane/DetectionSteph.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class DetectionSteph : MonoBehaviour
 {
     [SerializeField] private ObjectClass _objectClass;
-    [SerializeField] private Rigidbody _rb;
+    private Rigidbody _rb;
     [SerializeField] private int _nbObjectWaited;
     private int _nbObject = 0;
     public bool isComplet = false;
@@ -12,27 +12,28 @@ public class DetectionSteph : MonoBehaviour
     private void Verif()
     {
         _objectClass.interactType = ObjectType.Movable;
+        _rb = gameObject.AddComponent<Rigidbody>();
         _rb.constraints = RigidbodyConstraints.None;
         _rb.useGravity = true;
+        _rb.isKinematic = false;
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Stephane"))
         {
             Grab grab = other.gameObject.GetComponent<Grab>();
-            grab.DropObject();
-            other.transform.SetParent(transform);
             Vector3 target = other.gameObject.GetComponent<ObjectData>().target.transform.position;
+            grab.DropObject();
+            other.transform.position = target;
+            other.gameObject.GetComponent<ObjectClass>().interactType = ObjectType.None;
+            other.transform.SetParent(transform);
             Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
             rb.useGravity = false;
             rb.isKinematic = true;
-            other.gameObject.GetComponent<ObjectClass>().interactType = ObjectType.None;
-            other.transform.position = target;
             rb.constraints = RigidbodyConstraints.FreezeRotation;
             rb.constraints = RigidbodyConstraints.FreezePosition;
             _nbObject++;
-            Debug.Log(_nbObject);
-            Debug.Log(_nbObjectWaited);
+
             if (_nbObject == _nbObjectWaited)
             {
                 Verif();
