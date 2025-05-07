6f8266835cfa53f320a14feafac8ccca71ec7e84
Andréas <querat.andreas@gmail.com>
2025-05-07 17:04:06 +0200

    07/05/25
    Integration en cours !

--- Assets/Keran/Script/Final_Proto/objects/InteractionType/CodeManager.cs
+++ Assets/Keran/Script/Final_Proto/objects/InteractionType/CodeManager.cs
@@ -9,31 +9,50 @@ public class CodeManager : MonoBehaviour
     [HideInInspector] public bool isCorrect = false;
     [SerializeField] private TextMeshPro text;
 
+    [Header("UI Texts (optionnel)")]
+    [SerializeField] private TextMeshPro messageText;  // 3D TextMeshPro dans la scène;
+    [SerializeField] private string defaultMessage = "Entrez le code.";
+    [SerializeField] private string correctMessage = "Code correct !";
+    [SerializeField] private string incorrectMessage = "Code incorrect.";
+
+    private void Start()
+    {
+        if (messageText != null)
+            messageText.text = defaultMessage;
+    }
+
     public void AddToCode(int newValue)
     {
         if (!isCorrect && currentCode.Length < 4)
         {
             currentCode += newValue.ToString();
             text.text = currentCode;
-            StartCoroutine(VerifCode());
+
+            if (currentCode.Length == 4)
+            {
+                StartCoroutine(VerifCode());
+            }
         }
     }
 
     IEnumerator VerifCode()
     {
         yield return new WaitForSeconds(1);
-        if (currentCode.Length == 4)
+
+        if (currentCode == _finalCode)
         {
-            if (currentCode == _finalCode)
-            {
-                isCorrect = true;
-                text.text = "";
-            }
-            else
-            {
-                currentCode = "";
-                text.text = currentCode;
-            }
+            isCorrect = true;
+            text.text = "Correct";
+            if (messageText != null) messageText.text = correctMessage;
+        }
+        else
+        {
+            text.text = "False";
+            if (messageText != null) messageText.text = incorrectMessage;
+            yield return new WaitForSeconds(1);
+            currentCode = "";
+            text.text = "";
+            if (messageText != null) messageText.text = defaultMessage;
         }
     }
 }
